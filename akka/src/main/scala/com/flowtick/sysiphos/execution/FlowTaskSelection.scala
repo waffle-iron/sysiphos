f798677c6af217caad1a562470936f085ad82ca5
Marilyn Nowacka <marilyn.nowacka@fyber.com>
2019-06-05 14:40:12 +0200

    remove unused OnFailuretask from taskselection

--- akka/src/main/scala/com/flowtick/sysiphos/execution/FlowTaskSelection.scala
+++ akka/src/main/scala/com/flowtick/sysiphos/execution/FlowTaskSelection.scala
@@ -2,6 +2,5 @@ package com.flowtick.sysiphos.execution
 
 sealed trait FlowTaskSelection
 final case class TaskId(id: String) extends FlowTaskSelection
-final case class OnFailureId(id: String) extends FlowTaskSelection
 case object PendingTasks extends FlowTaskSelection
 
